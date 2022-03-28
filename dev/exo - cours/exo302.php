2c08ae95ccc27193ed6373a5791d00376f51c581
Adri3n-D1 <adri3n.d1@gmail.com>
2022-03-28 16:26:23 +0200

    record

--- dev/exo - cours/exo302.php
+++ dev/exo - cours/exo302.php
@@ -6,20 +6,24 @@
 
 // Créez un bloc try dans lequel :
 // • Stockez dans une variable 'connexion' une nouvelle instance de la classe 'PDO' avec comme arguments la source de la base de données (serveur) ainsi que les variables 'username' et 'password',
-// • Configurez l'attribut PDO qui sert à créer un rapport d’erreur pour qu’il émette une exception,
+// • Configurez l'attribut PDO qui sert à créer un rapport d’erreur pour qu’il emette une exception,
 // • Affichez le message « Connexion réussie<br> »,
 // • Fermez la connexion à la base de données.
 
 // Ajoutez ensuite un bloc pour attraper l’exception PDO dans lequel vous afficherez « Message d'erreur : [erreur]<br> ».
+
 $servername = 'localhost';
 $dbname = 'test';
 $username = 'root';
 $password = '';
 
 try {
-    $connexion = new PDO('mysql:host=' . $servername . ';dbname=' . $dbname, $username, $password);
-    echo $connexion;
+    $connexion = new PDO('mysql:host=' . $servername . ';dbname=' . $dbname . ';charset=utf8',
+    $username, $password,
+    [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+    echo 'Connexion réussie<br>';
+    $connexion = null;
 }
 catch (PDOException $e) {
-    echo $e->getMessage();
+    die('Message d\'erreur :' . $e->getMessage());
 }
